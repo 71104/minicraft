433948cc966986c7d32da1133f4579af9e898612
Alberto La Rocca <a71104@gmail.com>
2016-12-08 23:06:28 +0000

    Tests

--- test/Matrix.js
+++ test/Matrix.js
@@ -17,6 +17,29 @@ Matrix.prototype.verifyHeight = function () {
   return this._verifyHeight(this._root);
 };
 
+Matrix.prototype._verifyBST = function (i) {
+  if (i) {
+    var x = this._store.get(i, 'x');
+    var y = this._store.get(i, 'y');
+    var z = this._store.get(i, 'z');
+    var l = this._store.get(i, 'l');
+    if (l && this._cmp(x, y, z, l) >= 0) {
+      throw new Error();
+    }
+    var r = this._store.get(i, 'r');
+    if (r && this._cmp(x, y, z, r) <= 0) {
+      throw new Error();
+    }
+    return true;
+  } else {
+    return true;
+  }
+};
+
+Matrix.prototype.verifyBST = function () {
+  return this._verifyBST(this._root);
+};
+
 Matrix.prototype._verifyAVL = function (i) {
   if (i) {
     var b = this._height(this._store.get(i, 'r')) -
@@ -33,4 +56,10 @@ Matrix.prototype._verifyAVL = function (i) {
 
 Matrix.prototype.verifyAVL = function () {
   return this._verifyAVL(this._root);
+};
+
+Matrix.prototype.verifyAll = function () {
+  this.verifyHeight();
+  this.verifyBST();
+  this.verifyAVL();
 };
