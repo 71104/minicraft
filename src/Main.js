f9151d7fd517615312492d916cc8f8c930f52033
Alberto La Rocca <a71104@gmail.com>
2016-12-09 12:58:34 +0000

    Pipeline

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,67 @@
-var m = new Matrix();
+$(function () {
+  var canvas = $('canvas#canvas');
+  var width = canvas.width();
+  var height = canvas.height();
+  canvas.attr({
+    width: width,
+    height: height,
+  });
+  canvas = canvas.get(0);
 
-m.set(0, 0, 0, 1);
-m.set(1, 0, 0, 1);
-m.set(-1, 0, 0, 1);
-m.set(0, 1, 0, 1);
-m.set(0, -1, 0, 1);
+  var gl = canvas.getContext('webgl');
 
-console.dir(m.toJSON());
+  if (height > width) {
+    gl.viewport((width - height) / 2, 0, height, height);
+  } else {
+    gl.viewport(0, (height - width) / 2, width, width);
+  }
+
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Int16Array([
+      -1, 1, 1,
+      -1, -1, 1,
+      1, -1, 1,
+      1, -1, 1,
+      1, 1, 1,
+      -1, 1, 1,
+  ]), gl.DYNAMIC_DRAW);
+
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, $('script#fragment-shader').text());
+  gl.compileShader(fragmentShader);
+  if (gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.log(gl.getShaderInfoLog(fragmentShader));
+  } else {
+    console.error(gl.getShaderInfoLog(fragmentShader));
+  }
+
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, $('script#vertex-shader').text());
+  gl.compileShader(vertexShader);
+  if (gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.log(gl.getShaderInfoLog(vertexShader));
+  } else {
+    throw new Error(gl.getShaderInfoLog(vertexShader));
+  }
+
+  gl.enableVertexAttribArray(0);
+  gl.vertexAttribPointer(0, 3, gl.SHORT, false, 0, 0);
+
+  var program = gl.createProgram();
+  gl.attachShader(program, fragmentShader);
+  gl.attachShader(program, vertexShader);
+  gl.bindAttribLocation(program, 0, 'in_Vertex');
+  gl.linkProgram(program);
+  if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.log(gl.getProgramInfoLog(program));
+  } else {
+    throw new Error(gl.getProgramInfoLog(program));
+  }
+  gl.useProgram(program);
+
+  gl.clearColor(0, 0, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+  gl.flush();
+});
