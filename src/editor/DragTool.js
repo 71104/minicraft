f335797087c8b998ca5479bbb54351f86ba16fc0
Alberto La Rocca <a71104@gmail.com>
2016-12-21 19:57:13 +0100

    Drag tool

--- src/editor/DragTool.js
+++ src/editor/DragTool.js
@@ -1,8 +1,20 @@
 function DragTool(view, stage) {
   this._view = view;
   this._stage = stage;
+  this._x0 = 0;
+  this._y0 = 0;
 }
 
+DragTool.prototype.down = function (x, y) {
+  this._x0 = x;
+  this._y0 = y;
+  return false;
+};
+
 DragTool.prototype.drag = function (x, y) {
-  // TODO
+  this._view.x0 += this._x0 - x;
+  this._view.y0 += this._y0 - y;
+  this._x0 = x;
+  this._y0 = y;
+  return true;
 };
