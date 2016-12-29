8099f0aec179d6644ffbca835657aa757e3f2cd9
Alberto La Rocca <a71104@gmail.com>
2016-12-29 16:42:31 +0100

    Refactors Crosshair
    box.init
    
    box
    
    init/min/max
    
    x/y/z
    
    x_/y_/z_
    
    Procedures
    
    No objects

--- game/Crosshair.js
+++ game/Crosshair.js
@@ -2,8 +2,165 @@ function Crosshair(matrix, camera) {
   this._matrix = matrix;
   this._camera = camera;
   this._heading = {};
-  this._active = null;
+  this._box = {
+    init: {},
+    min: {},
+    max: {},
+  };
+  this._far = true;
+  this._active = {};
 }
+
+Crosshair.prototype._setActive = function (x, y, z) {
+  this._far = false;
+  this._active.x = x;
+  this._active.y = y;
+  this._active.z = z;
+  return true;
+};
+
+Crosshair.prototype._setFar = function () {
+  this._far = true;
+  return false;
+};
+
+Crosshair.prototype._pickLeft = function () {
+  const heading = this._heading;
+  const init = this._box.init;
+  const min = this._box.min;
+  const max = this._box.max;
+  if (heading.x < 0 && min.x >= init.x - 2) {
+    const position = this._camera.position;
+    const y = position.y + heading.y * (min.x - position.x) / heading.x;
+    const z = position.z + heading.z * (min.x - position.x) / heading.x;
+    if (y >= min.y && y <= max.y && z >= min.z && z <= max.z) {
+      const y_ = Math.floor(y);
+      const z_ = Math.floor(z);
+      if (this._matrix.has(y_, z_, min.x - 1)) {
+        return this._setActive(min.x - 1, y_, z_);
+      } else {
+        min.x--;
+        return this._pickLeft();
+      }
+    }
+  }
+  return this._pickRight();
+};
+
+Crosshair.prototype._pickRight = function () {
+  const heading = this._heading;
+  const init = this._box.init;
+  const min = this._box.min;
+  const max = this._box.max;
+  if (heading.x > 0 && max.x <= init.x + 3) {
+    const position = this._camera.position;
+    const y = position.y + heading.y * (max.x - position.x) / heading.x;
+    const z = position.z + heading.z * (max.x - position.x) / heading.x;
+    if (y >= min.y && y <= max.y && z >= min.z && z <= max.z) {
+      const y_ = Math.floor(y);
+      const z_ = Math.floor(z);
+      if (this._matrix.has(y_, z_, max.x)) {
+        return this._setActive(max.x, y_, z_);
+      } else {
+        max.x++;
+        return this._pickLeft();
+      }
+    }
+  }
+  return this._pickDown();
+};
+
+Crosshair.prototype._pickDown = function () {
+  const heading = this._heading;
+  const init = this._box.init;
+  const min = this._box.min;
+  const max = this._box.max;
+  if (heading.y < 0 && min.y >= init.y - 2) {
+    const position = this._camera.position;
+    const x = position.x + heading.x * (min.y - position.y) / heading.y;
+    const z = position.z + heading.z * (min.y - position.y) / heading.y;
+    if (x >= min.x && x <= max.x && z >= min.z && z <= max.z) {
+      const x_ = Math.floor(x);
+      const z_ = Math.floor(z);
+      if (this._matrix.has(min.y - 1, z_, x_)) {
+        return this._setActive(x_, min.y - 1, z_);
+      } else {
+        min.y--;
+        return this._pickLeft();
+      }
+    }
+  }
+  return this._pickUp();
+};
+
+Crosshair.prototype._pickUp = function () {
+  const heading = this._heading;
+  const init = this._box.init;
+  const min = this._box.min;
+  const max = this._box.max;
+  if (heading.y > 0 && max.y <= init.y + 3) {
+    const position = this._camera.position;
+    const x = position.x + heading.x * (max.y - position.y) / heading.y;
+    const z = position.z + heading.z * (max.y - position.y) / heading.y;
+    if (x >= min.x && x <= max.x && z >= min.z && z <= max.z) {
+      const x_ = Math.floor(x);
+      const z_ = Math.floor(z);
+      if (this._matrix.has(max.y, z_, x_)) {
+        return this._setActive(x_, max.y, z_);
+      } else {
+        max.y++;
+        return this._pickLeft();
+      }
+    }
+  }
+  return this._pickBack();
+};
+
+Crosshair.prototype._pickBack = function () {
+  const heading = this._heading;
+  const init = this._box.init;
+  const min = this._box.min;
+  const max = this._box.max;
+  if (heading.z < 0 && min.z >= init.z - 2) {
+    const position = this._camera.position;
+    const x = position.x + heading.x * (min.z - position.z) / heading.z;
+    const y = position.y + heading.y * (min.z - position.z) / heading.z;
+    if (x >= min.x && x <= max.x && y >= min.y && y <= max.y) {
+      const x_ = Math.floor(x);
+      const y_ = Math.floor(y);
+      if (this._matrix.has(y_, min.z - 1, x_)) {
+        return this._setActive(x_, y_, min.z - 1);
+      } else {
+        min.z--;
+        return this._pickLeft();
+      }
+    }
+  }
+  return this._pickFront();
+};
+
+Crosshair.prototype._pickFront = function () {
+  const heading = this._heading;
+  const init = this._box.init;
+  const min = this._box.min;
+  const max = this._box.max;
+  if (heading.z > 0 && max.z <= init.z + 3) {
+    const position = this._camera.position;
+    const x = position.x + heading.x * (max.z - position.z) / heading.z;
+    const y = position.y + heading.y * (max.z - position.z) / heading.z;
+    if (x >= min.x && x <= max.x && y >= min.y && y <= max.y) {
+      const x_ = Math.floor(x);
+      const y_ = Math.floor(y);
+      if (this._matrix.has(y_, max.z, x_)) {
+        return this._setActive(x_, y_, max.z);
+      } else {
+        max.z++;
+        return this._pickLeft();
+      }
+    }
+  }
+  return this._setFar();
+};
 
 Crosshair.prototype.pick = function () {
   const position = this._camera.position;
@@ -11,130 +168,23 @@ Crosshair.prototype.pick = function () {
   heading.x = -Math.sin(this._camera.angle.y);
   heading.y = Math.sin(this._camera.angle.x);
   heading.z = Math.cos(this._camera.angle.y) * Math.cos(this._camera.angle.x);
-  const x = Math.floor(position.x);
-  const y = Math.floor(position.y);
-  const z = Math.floor(position.z);
-  var x0 = Math.floor(position.x);
-  var y0 = Math.floor(position.y);
-  var z0 = Math.floor(position.z);
-  var x1 = Math.ceil(position.x);
-  var y1 = Math.ceil(position.y);
-  var z1 = Math.ceil(position.z);
-  while (true) {
-    if (heading.x < 0 && x0 >= x - 2) {
-      const y2 = position.y + heading.y * (x0 - position.x) / heading.x;
-      const z2 = position.z + heading.z * (x0 - position.x) / heading.x;
-      if (y2 >= y0 && y2 <= y1 && z2 >= z0 && z2 <= z1) {
-        const y3 = Math.floor(y2);
-        const z3 = Math.floor(z2);
-        if (this._matrix.has(y3, z3, x0 - 1)) {
-          return this._active = {
-            x: x0 - 1,
-            y: y3,
-            z: z3,
-          };
-        } else {
-          x0--;
-          continue;
-        }
-      }
-    }
-    if (heading.x > 0 && x1 <= x + 3) {
-      const y2 = position.y + heading.y * (x1 - position.x) / heading.x;
-      const z2 = position.z + heading.z * (x1 - position.x) / heading.x;
-      if (y2 >= y0 && y2 <= y1 && z2 >= z0 && z2 <= z1) {
-        const y3 = Math.floor(y2);
-        const z3 = Math.floor(z2);
-        if (this._matrix.has(y3, z3, x1)) {
-          return this._active = {
-            x: x1,
-            y: y3,
-            z: z3,
-          };
-        } else {
-          x1++;
-          continue;
-        }
-      }
-    }
-    if (heading.y < 0 && y0 >= y - 2) {
-      const x2 = position.x + heading.x * (y0 - position.y) / heading.y;
-      const z2 = position.z + heading.z * (y0 - position.y) / heading.y;
-      if (x2 >= x0 && x2 <= x1 && z2 >= z0 && z2 <= z1) {
-        const x3 = Math.floor(x2);
-        const z3 = Math.floor(z2);
-        if (this._matrix.has(y0 - 1, z3, x3)) {
-          return this._active = {
-            x: x3,
-            y: y0 - 1,
-            z: z3,
-          };
-        } else {
-          y0--;
-          continue;
-        }
-      }
-    }
-    if (heading.y > 0 && y1 <= y + 3) {
-      const x2 = position.x + heading.x * (y1 - position.y) / heading.y;
-      const z2 = position.z + heading.z * (y1 - position.y) / heading.y;
-      if (x2 >= x0 && x2 <= x1 && z2 >= z0 && z2 <= z1) {
-        const x3 = Math.floor(x2);
-        const z3 = Math.floor(z2);
-        if (this._matrix.has(y1, z3, x3)) {
-          return this._active = {
-            x: x3,
-            y: y1,
-            z: z3,
-          };
-        } else {
-          y1++;
-          continue;
-        }
-      }
-    }
-    if (heading.z < 0 && z0 >= z - 2) {
-      const x2 = position.x + heading.x * (z0 - position.z) / heading.z;
-      const y2 = position.y + heading.y * (z0 - position.z) / heading.z;
-      if (x2 >= x0 && x2 <= x1 && y2 >= y0 && y2 <= y1) {
-        const x3 = Math.floor(x2);
-        const y3 = Math.floor(y2);
-        if (this._matrix.has(y3, z0 - 1, x3)) {
-          return this._active = {
-            x: x3,
-            y: y3,
-            z: z0 - 1,
-          };
-        } else {
-          z0--;
-          continue;
-        }
-      }
-    }
-    if (heading.z > 0 && z1 <= z + 3) {
-      const x2 = position.x + heading.x * (z1 - position.z) / heading.z;
-      const y2 = position.y + heading.y * (z1 - position.z) / heading.z;
-      if (x2 >= x0 && x2 <= x1 && y2 >= y0 && y2 <= y1) {
-        const x3 = Math.floor(x2);
-        const y3 = Math.floor(y2);
-        if (this._matrix.has(y3, z1, x3)) {
-          return this._active = {
-            x: x3,
-            y: y3,
-            z: z1,
-          };
-        } else {
-          z1++;
-          continue;
-        }
-      }
-    }
-    return this._active = null;
-  }
+  const init = this._box.init;
+  const min = this._box.min;
+  const max = this._box.max;
+  init.x = Math.floor(position.x);
+  init.y = Math.floor(position.y);
+  init.z = Math.floor(position.z);
+  min.x = Math.floor(position.x);
+  min.y = Math.floor(position.y);
+  min.z = Math.floor(position.z);
+  max.x = Math.ceil(position.x);
+  max.y = Math.ceil(position.y);
+  max.z = Math.ceil(position.z);
+  return this._pickLeft();
 };
 
 Crosshair.prototype.isActive = function (x, y, z) {
-  return this._active &&
+  return !this._far &&
     this._active.x === x &&
     this._active.y === y &&
     this._active.z === z;
